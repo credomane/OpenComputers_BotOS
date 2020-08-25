13f0680121dbf0025e27c247c33231c5fe65b317
Credomane Evonguard <bb00200@gmail.com>
2020-08-25 17:33:45 -0500

    Stage 1 bootloader. Shamelessly taken from OpenOS.

---
+++ init.lua
@@ -0,0 +1,15 @@
+--Shamelessly taken from OpenOS.
+do
+    local addr, invoke = computer.getBootAddress(), component.invoke
+    local function loadfile(file)
+        local handle = assert(invoke(addr, "open", file))
+        local buffer = ""
+        repeat
+            local data = invoke(addr, "read", handle, math.huge)
+            buffer = buffer .. (data or "")
+        until not data
+        invoke(addr, "close", handle)
+        return load(buffer, "=" .. file, "bt", _G)
+    end
+    loadfile("/boot/kernel.lua")(loadfile)
+end
